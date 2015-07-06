9d3b31a9acdf96c8c2f3c00ac2ff7fae8457810b
kircheis0809 <kircheis0114@gmail.com>
2015-07-06 17:55:54 +0800

    首次將專案加入 Git 本地儲存庫

---
+++ HomeWorkWeekOne/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HomeWorkWeekOne.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        /// <summary>
+        /// 客戶資料管理
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Customer()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 客戶聯絡人管理
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult ContactPerson()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 客戶銀行帳戶管理
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Bank()
+        {
+            return View();
+        }
+    }
+}
---
+++ HomeWorkWeekOne/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HomeWorkWeekOne.MvcApplication" Language="C#" %>
